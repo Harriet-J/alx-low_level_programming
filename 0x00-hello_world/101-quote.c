71cc61c10c3dacffd20f3d6748b8157da21dc57b
Harriet-J <seremharriet@gmail.com>
2024-06-20 14:42:22 -0700

    number 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,13 +7,10 @@
  */
 int main(void)
 {
-    /* Message to be printed */
-    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    /* Using the write system call to print to stderr */
-    /* Parameters: file descriptor for stderr (2), pointer to message, length of message */
-    write(2, quote, 59);
+	write(2, quote, 59);
 
-    return (1); /* Return 1 to indicate success */
+	return (1);
 }
 
