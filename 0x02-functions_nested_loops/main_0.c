0daab91a2ef49e8100bf2ab7de67f9d1c5299f83
Harriet-J <seremharriet@gmail.com>
2024-07-05 12:26:59 -0700

    rot13

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,19 @@
+#include "main.h"  // Include the header file where print_to_98 is declared
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    // Test cases for print_to_98 function
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+
+    return (0);
+}
+
