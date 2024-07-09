53a84dc49a4cf1a19104eb7cb0919db8cbb42e2f
Harriet-J <seremharriet@gmail.com>
2024-07-09 13:07:52 -0700

    chess is amazing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,9 @@ char *_strcat(char *dest, const char *src);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);  /* Prototype for _strstr function */
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
+
 
 #endif /* MAIN_H */
 
