7e78af6e3d815766f7e24864cd2eb75bc68adc66
Harriet-J <seremharriet@gmail.com>
2024-07-09 12:46:42 -0700

    meh

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the main string to search in
+ * @needle: the substring to search for
+ *
+ * Return: pointer to the beginning of the located substring,
+ *         or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *start = haystack;
+		char *sub = needle;
+
+		while (*haystack && *sub && *haystack == *sub)
+		{
+			haystack++;
+			sub++;
+		}
+
+		if (!*sub)
+			return start;
+
+		haystack = start + 1;
+	}
+
+	return (NULL);
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-int _putchar(char c);
-char *_memcpy(char *dest, const char *src, unsigned int n);
-char *_strchr(char *s, char c);
+#include <stddef.h>
+
+/* Function prototypes */
+char *_strcat(char *dest, const char *src);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);  /* Prototype for _strstr function */
 
 #endif /* MAIN_H */
 
