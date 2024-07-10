48f5a650c81772616c5be340a4671af8a92b2543
Harriet-J <seremharriet@gmail.com>
2024-07-10 15:03:15 -0700

    1

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,20 +8,21 @@
  */
 int is_prime_number(int n)
 {
+    int i;
     /* Handle cases for non-prime numbers */
     if (n <= 1)
-        return 0;
+        return (0);
     if (n <= 3)
         return 1;
     if (n % 2 == 0 || n % 3 == 0)
-        return 0;
+        return (0);
 
     /* Check for prime numbers in remaining cases */
-    for (int i = 5; i * i <= n; i += 6)
+    for (i = 5; i * i <= n; i += 6)
     {
         if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
+            return (0);
     }
-    return 1;
+    return (1);
 }
 
