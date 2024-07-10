c5dc07eeee9e8ea5df819d6e3a4a47371c5effe0
Harriet-J <seremharriet@gmail.com>
2024-07-10 14:52:07 -0700

    fin

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,9 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (find_sqrt(n, 0));
+    if (n < 0)
+        return (-1);
+    return (find_sqrt(n, 0));
 }
 
 /**
@@ -22,10 +22,10 @@ int _sqrt_recursion(int n)
  */
 int find_sqrt(int n, int i)
 {
-	if (i * i == n)
-		return (i);
-	if (i * i > n)
-		return (-1);
-	return (find_sqrt(n, i + 1));
+    if (i * i == n)
+        return (i);
+    if (i * i > n)
+        return (-1);
+    return (find_sqrt(n, i + 1));
 }
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _putchar(char c);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int find_sqrt(int n, int i);
+int is_prime_number(int n);
 
 #endif /* MAIN_H */
 
