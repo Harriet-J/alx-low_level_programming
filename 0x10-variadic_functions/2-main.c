ab04c6bd13e31b3ed60c3d61692097952655b195
Harriet-J <seremharriet@gmail.com>
2024-07-26 17:55:09 -0700

    constant

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - Check the code for print_strings function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
+
