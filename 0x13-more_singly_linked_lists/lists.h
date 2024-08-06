e9125b84d9706a10b55623335350d1979adaab38
Harriet-J <seremharriet@gmail.com>
2024-08-06 13:18:22 -0700

    free list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *add_nodeint_end(listint_t **head, const int n);  
 listint_t *add_nodeint_end(listint_t **head, const int n); /* Add this line */
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 
 #endif /* LISTS_H */
