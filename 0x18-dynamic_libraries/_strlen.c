8cf959399f29664d91fa3bccf28cb883a23d33cb
Harriet-J <seremharriet@gmail.com>
2024-09-23 14:21:45 -0700

    advanced

--- 0x18-dynamic_libraries/_strlen.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-int _strlen(char *s) {
-    /* Use s to avoid the unused parameter warning */
-    (void)s; /* This explicitly tells the compiler we're not using s yet */
-    return 0; /* Placeholder return value */
-}
-
